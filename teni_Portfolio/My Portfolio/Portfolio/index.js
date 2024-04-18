26b3544094076bc5132c8bb1ab7a9ca530b737f1
Omoteni19 <136773664+Omoteni19@users.noreply.github.com>
2024-04-18 17:09:07 +0100

    Add files via upload

---
+++ teni_Portfolio/My Portfolio/Portfolio/index.js
@@ -0,0 +1,21 @@
+"use strict";
+let menu = document.getElementById("menu");
+let navModal = document.getElementById("navigationLinksContainerTwo");
+let body = document.getElementById("body");
+let closeIcon = document.getElementById("closeIcon");
+let checkState = true;
+let showModal = function () {
+    if (checkState === true) {
+        navModal.classList.remove("navigationLinksContainerTwo");
+        navModal.classList.add("navigationLinksContainerTwoVisible");
+        checkState = false;
+    }
+    else if (checkState === false) {
+        navModal.classList.remove("navigationLinksContainerTwoVisible");
+        navModal.classList.add("navigationLinksContainerTwo");
+        checkState = true;
+    }
+};
+
+menu.addEventListener("click", showModal);
+closeIcon.addEventListener("click", showModal);
